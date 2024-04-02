57162c850a5782fdbce5f81eb3a0e407994d37f4
JersonVitor <jersonvitor2016@gmail.com>
2024-04-02 14:06:36 -0300

    70% dos arquivos

---
+++ app/src/main/java/com/jerson/gymapp/activities/TreinoActivity.kt
@@ -0,0 +1,21 @@
+package com.jerson.gymapp.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jerson.gymapp.R
+
+class TreinoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_treino)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/jerson/gymapp/adapter/TreinoAdapter.kt
@@ -0,0 +1,4 @@
+package com.jerson.gymapp.adapter
+
+class TreinoAdapter {
+}
